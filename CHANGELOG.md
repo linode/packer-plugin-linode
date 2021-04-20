59a213f18710cad00a0d1e6ae8e8e8a827a2333d
Megan Marsh <megan@hashicorp.com>
2021-04-20 16:08:35 -0700

    clean up builders that got swept in via historical filter, tweak to get it compiling, rewrite acceptance tests

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 (Unreleased)
+
+* Update acceptance tests to use new packer-plugin-sdk/acctest framework
--- builder/linode/builder_acc_test.go
+++ builder/linode/builder_acc_test.go
@@ -1,30 +1,43 @@
 package linode
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
-	builderT "github.com/hashicorp/packer/acctest"
+	"github.com/hashicorp/packer-plugin-sdk/acctest"
 )
 
 func TestBuilderAcc_basic(t *testing.T) {
-	builderT.Test(t, builderT.TestCase{
-		PreCheck: func() { testAccPreCheck(t) },
-		Builder:  &Builder{},
+	if skip := testAccPreCheck(t); skip == true {
+		return
+	}
+	acctest.TestPlugin(t, &acctest.PluginTestCase{
+		Name:     "test-linode-builder-basic",
+		Type:     "linode",
 		Template: testBuilderAccBasic,
 	})
 }
 
-func testAccPreCheck(t *testing.T) {
+func testAccPreCheck(t *testing.T) bool {
+	if os.Getenv(acctest.TestEnvVar) == "" {
+		t.Skip(fmt.Sprintf(
+			"Acceptance tests skipped unless env '%s' set",
+			acctest.TestEnvVar))
+		return true
+	}
+
 	if v := os.Getenv("LINODE_TOKEN"); v == "" {
 		t.Fatal("LINODE_TOKEN must be set for acceptance tests")
+		return true
 	}
+	return false
 }
 
 const testBuilderAccBasic = `
 {
 	"builders": [{
-		"type": "test",
+		"type": "linode",
 		"region": "us-east",
 		"instance_type": "g6-nanode-1",
 		"image": "linode/alpine3.9",
