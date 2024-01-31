c36cfc81926925381738de00cc5e19d6b5928585
Youjung Kim <126618609+ykim-1@users.noreply.github.com>
2024-01-31 10:49:48 -0700

    test: Add e2e test framework and update makefile and GHA workflow (#185)
    * Add unit test coverage and add workflow to upload test data
    
    * e2e framework, update makefile, tod upload logic changed to using git submodule repo
    
    * oops forgot LINODE_TOKEN
    
    * add submodule in workflow file
    
    * misplaced submodule
    
    * lint
    
    * rename GNUmakefile to Makefile
    
    * Add teardown
    
    * defer teardown method
    
    * place defer block
    
    * made image label as global variable for duplicating tests
    
    * go fmt and update variable names
    
    ---------
    
    Co-authored-by: Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>

---
+++ test/integration/e2e_test.go
@@ -0,0 +1,135 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+)
+
+const (
+	packerTemplate = "template/test_image_template.json"
+)
+
+func TestBuildPackerImage(t *testing.T) {
+	linodeToken := os.Getenv("LINODE_TOKEN")
+
+	if linodeToken == "" {
+		t.Fatal("Linode token is not set. Please set LINODE_TOKEN as environment variable.")
+	}
+
+	linodeImageLabel := generateImageLabel()
+	err := os.Setenv("LINODE_IMAGE_LABEL", linodeImageLabel)
+	if err != nil {
+		fmt.Printf("Error setting LINODE_IMAGE_LABEL: %v\n", err)
+		return
+	}
+
+	// Run the Packer build command from terminal
+	cmd := exec.Command("packer", "build", packerTemplate)
+
+	output, err := cmd.CombinedOutput()
+
+	defer func() {
+		if err := teardown(linodeImageLabel); err != nil {
+			fmt.Printf("Error during deleting image after test execution: %v\n", err)
+		}
+	}()
+
+	// Check if the Packer build was successful
+	if err != nil {
+		t.Fatalf("Error building Packer image: %v\nOutput:\n%s", err, output)
+	}
+
+	// Assert the output contains expected strings
+	expectedSubstring := "Builds finished. The artifacts of successful builds"
+	assert.True(t, strings.Contains(string(output), expectedSubstring), "Expected successful build output to contain: %s", expectedSubstring)
+
+	// Assert other fields
+	err = assertLinodeImage(linodeImageLabel, t)
+
+	if err != nil {
+		t.Fatalf("Error asserting Linode builder image: %v", err)
+	}
+}
+
+func assertLinodeImage(imageLabel string, t *testing.T) error {
+	client := getLinodegoClient()
+
+	images, err := client.ListImages(context.Background(), nil)
+	if err != nil {
+		return fmt.Errorf("error listing Linode images: %v", err)
+	}
+
+	// Find the desired image by label prefix
+	var targetImage *linodego.Image
+	for _, image := range images {
+		if image.Label != "" && strings.HasPrefix(image.Label, imageLabel) {
+			targetImage = &image
+			break
+		}
+	}
+
+	if targetImage == nil {
+		return fmt.Errorf("image with label '%s' not found", imageLabel)
+	}
+
+	assert.Equal(t, "manual", targetImage.Type, "unexpected instance type")
+	expectedInstanceIDFormat := "private/"
+	assert.True(t, strings.HasPrefix(targetImage.ID, expectedInstanceIDFormat), "unexpected instance ID prefix")
+	expectedInstanceLabel := "test-image-"
+	assert.True(t, strings.HasPrefix(targetImage.Label, expectedInstanceLabel), "unexpected instance label prefix")
+	expectedImageDescription := "My Test Image Description"
+	assert.Equal(t, expectedImageDescription, targetImage.Description, "unexpected image description")
+
+	return nil
+}
+
+func teardown(imageLabel string) error {
+	client := getLinodegoClient()
+	images, err := client.ListImages(context.Background(), nil)
+	if err != nil {
+		return fmt.Errorf("error listing Linode images: %v", err)
+	}
+
+	for _, image := range images {
+		if image.Label != "" && strings.HasPrefix(image.Label, imageLabel) {
+			err = client.DeleteImage(context.Background(), image.ID)
+			if err != nil {
+				return fmt.Errorf("error during Linode image deletion: %v", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func getLinodegoClient() linodego.Client {
+	linodeToken := os.Getenv("LINODE_TOKEN")
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: linodeToken})
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+	linodeClient := linodego.NewClient(oauth2Client)
+
+	return linodeClient
+}
+
+func generateImageLabel() string {
+	timestamp := strconv.FormatInt(time.Now().UnixNano(), 10) // Shortened format without dashes
+	instanceLabel := fmt.Sprintf("test-image-%s", timestamp)
+
+	return instanceLabel
+}
