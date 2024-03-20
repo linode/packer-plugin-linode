638f175765896194191d4a377827daa69a68c8dc
Zhiwei Liang <zliang@akamai.com>
2024-03-20 01:33:52 -0400

    Add go doc for LinodeCommon

--- helper/common.go
+++ helper/common.go
@@ -1,6 +1,7 @@
 //go:generate packer-sdc struct-markdown
 package helper
 
+// The common configuration options related to Linode services
 type LinodeCommon struct {
 	// The Linode API token. This can also be specified in LINODE_TOKEN environment variable
 	PersonalAccessToken string `mapstructure:"linode_token"`
