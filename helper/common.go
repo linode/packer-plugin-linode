b7467e29f565d9c851d98ef56fb3f160a251e6a1
Zhiwei Liang <zliang@akamai.com>
2024-05-08 13:04:48 -0400

    Add token scopes requirements

--- helper/common.go
+++ helper/common.go
@@ -7,5 +7,7 @@ type LinodeCommon struct {
 	// This can also be specified in `LINODE_TOKEN` environment variable.
 	// Saving the token in the environment or centralized vaults
 	// can reduce the risk of the token being leaked from the codebase.
+	// `images:read_write`, `linodes:read_write`, and `events:read_only`
+	// scopes are required for the API token.
 	PersonalAccessToken string `mapstructure:"linode_token"`
 }
